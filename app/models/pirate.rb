6cd6e7313419607e3d30ee974573b54cc9ef968e
Tyler Sell <tylerselldesign@gmail.com>
2019-02-13 06:27:51 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+  
+  attr_accessor :name, :weight, :height
+  
+  PIRATES = []
+  
+  def inititialize(name, weight, height)
+    @name = name 
+    @weight = weight
+    @height = height
+    PIRATES << self 
+  end
+  
+  def self.all 
+    PIRATES
+  end
+  
 end
